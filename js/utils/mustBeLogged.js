a9de9299b21b1c291ca09a56e4de7e15cdee791f
Felichz <andersson.fx@gmail.com>
2020-08-19 21:48:27 -0300

    Fix login

--- js/utils/mustBeLogged.js
+++ js/utils/mustBeLogged.js
@@ -1,5 +1,5 @@
-// firebase.auth().onAuthStateChanged((user) => {
-//     if (!user) {
-//         window.location = 'login.html';
-//     }
-// });
+firebase.auth().onAuthStateChanged((user) => {
+    if (!user) {
+        window.location = 'login.html';
+    }
+});
